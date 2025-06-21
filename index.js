3b4f934d802df75314dab4f5824e1cf99b98533c
Georgiana1788 <162391781+Georgiana1788@users.noreply.github.com>
2025-06-21 14:22:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const http = require('http');
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from server!');
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});const express = require('express');
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
